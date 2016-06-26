7edb3366c515de58a84aa7d05c991627805dfec7
NSilv <n.silvestrin98@gmail.com>
2016-06-26 18:54:25 +0200

    Implemented most remaining tags

--- tags.js
+++ tags.js
@@ -7,41 +7,82 @@ var bbTags = {};
 bbTags["b"] = new BBTag("b", true, false, false);
 bbTags["i"] = new BBTag("i", true, false, false);
 bbTags["u"] = new BBTag("u", true, false, false);
+bbTags["s"] = new BBTag("s", true, false, false);
 
 //Useless?
-bbTags["text"] = new BBTag("text", true, false, true, (tag, content, attr) => {
-  return content;
+bbTags["text"] = new BBTag("text", true, false, true, function(tag, content, attr) {
+    return content;
 });
 
+//Font size
+bbTags["size"] = new BBTag("size", true, false, false, function(tag, content, attr) {
+    return '<span style="font-size:' + attr + '">' + content + '</font>';
+});
+
+//Font color
+bbTags["color"] = new BBTag("color", true, false, false, function(tag, content, attr) {
+    return '<span style="color:' + attr + '">' + content + '</font>';
+});
+
+//Text align : center
+bbTags["center"] = new BBTag("center", true, false, false, function(tag, content, attr) {
+    return '<div style="text-align:center">' + content + '</div>';
+});
+
+//Quote
+bbTags["quote"] = new BBTag("quote", true, true, false, function(tag, content, attr) {
+    return '<blockquote><p>' + content + '</p></blockquote>';
+});
+
+
+//Un-ordered list
+bbTags["list"] = new BBTag("ul", true, true, false, function(tag, content, attr) {
+    return '<ul>' + content + '</ul>';
+});
+
+//Ordered list
+bbTags["ol"] = new BBTag("ul", true, true, false, function(tag, content, attr) {
+    return '<ol>' + content + '</ol>';
+});
+
+//List item
+bbTags["li"] = new BBTag("li", true, false, false, function(tag, content, attr) {
+    return '<li>' + content + '</li>';
+});
+
+//Shorthand for list item
+bbTags["*"] = bbTags["li"];
+
+
 //Image
-bbTags["img"] = new BBTag("img", true, false, false, (tag, content, attr) => {
-  return "<img src=\"" + content + "\" />";
+bbTags["img"] = new BBTag("img", true, false, false, function(tag, content, attr) {
+    return "<img src=\"" + content + "\" />";
 });
 
 //Link
-bbTags["url"] = new BBTag("url", true, false, false, (tag, content, attr) => {
-  var link = content;
+bbTags["url"] = new BBTag("url", true, false, false, function(tag, content, attr) {
+    var link = content;
 
-  if (attr["url"] != undefined) {
-    link = escapeHTML(attr["url"]);
-  }
+    if (attr["url"] != undefined) {
+        link = escapeHTML(attr["url"]);
+    }
 
-  if (!startsWith(link, "http://") && !startsWith(link, "https://")) {
-    link = "http://" + link;
-  }
+    if (!startsWith(link, "http://") && !startsWith(link, "https://")) {
+        link = "http://" + link;
+    }
 
-  return "<a href=\"" + link + "\" target=\"_blank\">" + content + "</a>";
+    return "<a href=\"" + link + "\" target=\"_blank\">" + content + "</a>";
 });
 
 //Code highlighting
-bbTags["code"] = new BBTag("code", true, false, true, (tag, content, attr) => {
-  var lang = attr["lang"];
+bbTags["code"] = new BBTag("code", true, false, true, function(tag, content, attr) {
+    var lang = attr["lang"];
 
-  if (lang !== undefined) {
-    return "<code class=\"" + escapeHTML(lang) + "\">" + content + "</code>";
-  } else {
-    return "<code>" + content + "</code>";
-  }
+    if (lang !== undefined) {
+        return "<code class=\"" + escapeHTML(lang) + "\">" + content + "</code>";
+    } else {
+        return "<code>" + content + "</code>";
+    }
 });
 
 module.exports = bbTags;
